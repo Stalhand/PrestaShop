69bec09f53a387083b964c3faddfe97f4a8c0c5e
Helena Stalhand <helena@stalhand.se>
2020-02-13 10:11:04 +0100

    first commit

---
+++ src/test/java/PagesPresta/PrestaShopTest1.java
@@ -0,0 +1,24 @@
+package PagesPresta;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PrestaShopTest1 {
+
+static WebDriver driver = new ChromeDriver();
+
+    @BeforeClass
+    public static void setup() {
+        System.out.println("BeforeClass -- setup");
+        driver.get("http://demo.prestashop.com");
+
+    }
+
+    @Test
+    public void signIn(){
+
+
+    }
+}
